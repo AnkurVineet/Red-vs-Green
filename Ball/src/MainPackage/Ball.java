c88e1cbff12a034bb211531dfd6381ce72dd20df
Ankur Vineet <vineetankuro2@gmail.com>
2018-06-29 23:42:03 +0530

    Java Class Direct to github.com

---
+++ Ball/src/MainPackage/Ball.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainPackage;
+/**
+ *
+ * @author AnkurVineet
+ */
+public class Ball 
+{
+    Ball()
+    {
+        System.out.println("Direct Push to Github.com");
+    }
+}
