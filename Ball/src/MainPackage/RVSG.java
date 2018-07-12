0b0a097c7a772b82a9a86f5416c2131cc6567737
Ankur Vineet <vineetankuro2@gmail.com>
2018-07-13 02:25:51 +0530

    Boundary Added.

--- Ball/src/MainPackage/RVSG.java
+++ Ball/src/MainPackage/RVSG.java
@@ -24,9 +24,12 @@ public class RVSG extends JFrame implements KeyListener
 {
     Ball b1,b2;
     Timer timer;
+    static int screen_height = 0;
+    static int screen_width = 0;
     int STEP_SIZE = 3;
     int BALL_RADIUS = 60;
     boolean leftP1=false,rightP1=false,upP1=false,downP1=false;
+    boolean leftP2=false,rightP2=false,upP2=false,downP2=false;
         RVSG() 
 	{
             b1 = new Ball(BALL_RADIUS, 'R', 50, 50);
@@ -48,8 +51,8 @@ public class RVSG extends JFrame implements KeyListener
 		RVSG gamepage = new RVSG();
                 gamepage.setTitle("RED V/S GREEN");
                 Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-                int height = (int)screenSize.getHeight();
-                int width =  (int)screenSize.getWidth();
+                screen_height = (int)screenSize.getHeight();
+                screen_width =  (int)screenSize.getWidth();
 		//gamepage.setSize(width,height);
                 gamepage.setExtendedState(MAXIMIZED_BOTH);
 		gamepage.setVisible(true);
@@ -78,8 +81,24 @@ public class RVSG extends JFrame implements KeyListener
             b1.ypos += STEP_SIZE;
         if(rightP1)
             b1.xpos += STEP_SIZE;
-        System.out.println("\t"+upP1);
-        System.out.println(leftP1+":"+downP1+":"+rightP1);
+        if(leftP2)
+            b2.xpos -= STEP_SIZE;
+        if(upP2)
+            b2.ypos -= STEP_SIZE;
+        if(downP2) 
+            b2.ypos += STEP_SIZE;
+        if(rightP2)
+            b2.xpos += STEP_SIZE;
+        if(b1.xpos < 5 ) b1.xpos = 5;
+        if(b2.xpos < 5 ) b2.xpos = 5;
+        if(b1.ypos < 30 ) b1.ypos = 30;
+        if(b2.ypos < 30 ) b2.ypos = 30;
+        if(b1.xpos + 50 > screen_width ) b1.xpos = screen_width - 50;
+        if(b2.xpos + 50 > screen_width ) b2.xpos = screen_width - 50;
+        if(b1.ypos + 90 > screen_height ) b1.ypos = screen_height - 90;
+        if(b2.ypos + 90 > screen_height ) b2.ypos = screen_height - 90;
+        //System.out.println("\t"+upP1);
+        //System.out.println(leftP1+":"+downP1+":"+rightP1);
         repaint();
     });
     timer.setRepeats(true);
@@ -90,27 +109,22 @@ public class RVSG extends JFrame implements KeyListener
     @Override
     public void keyPressed(KeyEvent e) 
     {
-        
         if(e.getKeyCode() == KeyEvent.VK_W)
-        {
-            //System.out.println("W pressed");
             upP1 = true;
-        }
         if(e.getKeyCode() == KeyEvent.VK_A)
-        {
-            //System.out.println("A pressed");
             leftP1 = true;
-        }
         if(e.getKeyCode() == KeyEvent.VK_S)
-        {
-            //System.out.println("S pressed");
             downP1 = true;
-        }
         if(e.getKeyCode() == KeyEvent.VK_D)
-        {
-            //System.out.println("D pressed");
             rightP1 = true;
-        }
+        if(e.getKeyCode() == KeyEvent.VK_NUMPAD5)
+            upP2 = true;
+        if(e.getKeyCode() == KeyEvent.VK_NUMPAD1)
+            leftP2 = true;
+        if(e.getKeyCode() == KeyEvent.VK_NUMPAD2)
+            downP2 = true;
+        if(e.getKeyCode() == KeyEvent.VK_NUMPAD3)
+            rightP2 = true;
     }
 
      @Override
@@ -122,22 +136,22 @@ public class RVSG extends JFrame implements KeyListener
     @Override
     public void keyReleased(KeyEvent e) 
     {
-         if(e.getKeyCode() == KeyEvent.VK_W)
-        {
+        if(e.getKeyCode() == KeyEvent.VK_W)
             upP1 = false;
-        }
         if(e.getKeyCode() == KeyEvent.VK_A)
-        {
             leftP1 = false;
-        }
         if(e.getKeyCode() == KeyEvent.VK_S)
-        {
             downP1 = false;
-        }
         if(e.getKeyCode() == KeyEvent.VK_D)
-        {
             rightP1 = false;
-        }
+        if(e.getKeyCode() == KeyEvent.VK_NUMPAD5)
+            upP2 = false;
+        if(e.getKeyCode() == KeyEvent.VK_NUMPAD1)
+            leftP2 = false;
+        if(e.getKeyCode() == KeyEvent.VK_NUMPAD2)
+            downP2 = false;
+        if(e.getKeyCode() == KeyEvent.VK_NUMPAD3)
+            rightP2 = false;
     }
 
 }
